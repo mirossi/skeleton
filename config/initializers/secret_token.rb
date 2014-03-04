463be74ec577652338927bd090bf4f253cdf262e
Mirco <mirco.rossi@gmail.com>
2014-03-04 14:57:09 +0100

    saFD :I

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Skeleton::Application.config.secret_key_base = '2145d261142b2b2bcdd9254dbe85aba11d5a4bc33cef824a0f3334ff86620adbefd93308a807594d39425d8de68075ce62982ffcccd6ecf1ca6ece5fa521fcdb'
