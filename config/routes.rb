2c36cce36e193b03419ea27aaec3b841144b97cb
Mirco <mirco.rossi@gmail.com>
2014-03-08 18:54:17 +0000

    bevor tests

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Skeleton::Application.routes.draw do
 
 
+  resources :animals
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140307234141) do
+ActiveRecord::Schema.define(version: 20140308183958) do
+
+  create_table "animals", force: true do |t|
+    t.string   "name"
+    t.integer  "maxAge"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "comments", force: true do |t|
     t.string   "content"
