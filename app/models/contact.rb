adce7351032b92e82e216db5542ad2fa7f3ded8a
Mirco <mirco.rossi@gmail.com>
2014-03-14 23:24:49 +0100

    Modelled with hasContact

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -3,8 +3,8 @@ class Contact < ActiveRecord::Base
   validates :zip, length: { minimum: 3 }
   validates :city, length: { minimum: 3 }
   validates :mobile, length: { minimum: 3 }
-  
-  belongs_to :person
-  
+
+  has_one :person_has_contact
+  has_one :person, through: :person_has_contact
   accepts_nested_attributes_for :person
 end
